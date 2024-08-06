c9d146b85a22635b833e840f51b438de1e301761
Chandana R J <117065228+Chandanarj@users.noreply.github.com>
2024-08-06 12:20:41 +0530

    Update conftest.py

--- selenium-python-pom/conftest.py
+++ selenium-python-pom/conftest.py
@@ -1,11 +1,14 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.support.events import EventFiringWebDriver
+from MyListner import MyListner
 from conf import Credential
 from pages.login_page import LoginPage
 
+
 @pytest.fixture(scope="class")
 def set_up(request, browser_type):
-    print("Running on browser: " + browser_type)
+    print("Running on brower : "+browser_type)
     if browser_type == 'chrome':
         options = webdriver.ChromeOptions()
         options.add_experimental_option("detach", True)
@@ -14,20 +17,25 @@ def set_up(request, browser_type):
         driver = webdriver.Firefox()
     elif browser_type == 'edge':
         driver = webdriver.Edge()
-    else:
-        raise ValueError(f"Unsupported browser type: {browser_type}")
     driver.implicitly_wait(10)
+    #driver = EventFiringWebDriver(driver, MyListner())
     request.cls.driver = driver
     yield
     driver.quit()
 
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+
 @pytest.fixture(scope="class", autouse=True)
 def browser_type(request):
-    return request.config.getoption("--browser", default="chrome")
+    return request.config.getoption("--browser")
 
 @pytest.fixture()
 def navigate_url(request):
     request.cls.driver.get(Credential["Url"])
+
 
 @pytest.fixture()
 def login_logout(request):
